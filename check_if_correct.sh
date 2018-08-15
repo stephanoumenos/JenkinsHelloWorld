5f29220af32bb5c8fa19335360a7d5e8a7d1616d
stephanoumenos <s_dechecchi@hotmail.com>
2018-08-15 14:53:03 -0300

    Changed to echo the program output

--- check_if_correct.sh
+++ check_if_correct.sh
@@ -3,6 +3,10 @@
 
 program_output=$(java -cp HelloWorldJava/target/HelloWorldJava-1.0-SNAPSHOT.jar com.stephanoumenos.helloworld.App)
 
+echo Program output:
+echo $program_output
+echo 
+
 
 if [ "$program_output" == "Hello World!" ]; then
     echo Program executed correctly
